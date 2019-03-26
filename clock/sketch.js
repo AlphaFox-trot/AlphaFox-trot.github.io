f8bc61d87d32e94d7ee4c24c3887a2b93507d663
AlphaFox-trot <bertrandrew@gmail.com>
2019-03-26 15:20:10 -0600

    worked on clock

--- clock/sketch.js
+++ clock/sketch.js
@@ -5,20 +5,39 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let clockSize;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(200);
+  angleMode(DEGREES);
 }
 
 function draw() {
+  createCanvas(windowWidth, windowHeight);
+  background(200);
   translate(windowWidth/2, windowHeight/2);
+  clockSize = 600;
+  generateClock();
+  generateArms();
+}
+
+function generateClock(){
   fill(225, 225, 225);
-  ellipse(600, 600, 0, 0);
+  ellipse(0, 0, clockSize, clockSize);
+  fill(0, 0, 0);
+  ellipse(0, 0, 3, 3);
+  rectMode(CENTER);
   for (let x = 0; x < 12; x += 1) {
+    rect(245, 0, 75, 5);
+    rotate (6);
     for (let y = 0; y < 4; y += 1){
-      rect(300, 0, 2, 10);
+      rect(250, 0, 50, 1);
       rotate (6);
     }
   }
+}
+
+function generateArms(){
+  push();
+  
+  pop();
 }
