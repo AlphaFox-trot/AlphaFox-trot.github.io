2ff89bfe02ca94a252696e4d0850200148e2317d
AlphaFox-trot <bertrandrew@gmail.com>
2019-05-14 15:20:12 -0600

    worked on circle

--- Circle/sketch.js
+++ Circle/sketch.js
@@ -5,11 +5,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let firstCircle = {
+  x: 400,
+  y: 400, 
+  radius: 600
+};
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
+}
+
+function drawcircles(point, degree){
+
+
 }
